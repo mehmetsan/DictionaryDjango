d37e56e042ec81198e51cd11351f9a05fd1dd605
mehmetsan <trabzonpower@gmail.com>
2021-02-08 18:00:49 +0300

    WordsAPI adds word on the home page

--- dictApp/home/views.py
+++ dictApp/home/views.py
@@ -9,10 +9,12 @@ from dotenv import load_dotenv
 
 # Create your views here.
 def home(request):
+    
+    current_user = request.user
+    user_words = Dictword.objects.all().filter(user=current_user)
+
 
     if request.method == "POST":
-
-        load_dotenv('.env')
 
         load_dotenv('.env')
 
@@ -30,16 +32,14 @@ def home(request):
 
         first_meaning = json_data['definitions'][0]
 
-        word, partOfSpeech = first_meaning['definition'], first_meaning['partOfSpeech']
+        definition, partOfSpeech = first_meaning['definition'], first_meaning['partOfSpeech']
 
-
-
-
-
-    current_user = request.user
-    user_words = Dictword.objects.all().filter(user=current_user)
-
-
+        Dictword.objects.create(
+            user = current_user,
+            word = search_word,
+            definition = definition,
+            partOfSpeech = partOfSpeech
+        )
 
 
     return render( request, 'home/home.html', {'words':user_words} )
