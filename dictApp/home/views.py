47f4133743b1231415d40d22aeb4cf00e86d27a6
mehmetsan <trabzonpower@gmail.com>
2021-02-09 00:38:10 +0300

    Practice check implemented

--- dictApp/home/views.py
+++ dictApp/home/views.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 
 def get_data(search_word):
-    # API CONFIGURATION
+    # API Configuration
     url = "https://wordsapiv1.p.rapidapi.com/words/" + search_word + "/definitions"
 
     headers = {
@@ -23,7 +23,7 @@ def get_data(search_word):
 
     first_meaning = json_data['definitions'][0]
 
-    # STORING ONLY THE DEFINITION AND THE PART OF SPEECH
+    # Storing only the definition and part of speech
     definition, part_of_speech = first_meaning['definition'], first_meaning['partOfSpeech']
 
     return definition, part_of_speech
@@ -40,63 +40,69 @@ def home(request):
     
     current_user = User.objects.all().filter(username=request.user.username)[0]
     all_words = Dictword.objects.all()
-    user_words = all_words.filter(user=current_user)
-
+    user_words = all_words.filter(user=current_user).order_by('-id')
+    practice = True if len(user_words) > 9 else False
 
     if request.method == "POST":
 
-        #  USING ENVIRONMENT VARIABLE FOR API KEY
+        #  Using environment variables for API KEY
         load_dotenv('.env')
 
         search_word = request.POST.get('search_word')
 
         global_dict_check = global_check(search_word)
 
-        # IF SEARCHING A NEW WORD
+        # If searching a new word
         if search_word:
             definition, part_of_speech = "", ""
-            
+
             if global_dict_check:
                 definition = global_dict_check.definition
                 part_of_speech = global_dict_check.part_of_speech
             else:
                 definition, part_of_speech = get_data(search_word)
 
-            # CONVERT INTO ONE SENTENCE, SO THAT IT CAN BE STORED IN THE HIDDEN INPUT FIELD
+            # Convert definition to one sentence to store its value in hidden input
             def_one_word = definition.replace(' ','-')
 
-            # FOR DISPLAYING THE SEARCHED WORD INFO
+            # For displaying the Searched Word information
             word_data = {
                 'word' : search_word.capitalize(),
                 'definition' : definition.capitalize(),
                 'part_of_speech' : part_of_speech.capitalize()
             }
-            
-            return render( request, 'home/home.html', {'words':user_words, 'made_a_search':True, "query":word_data, "def_one_word":def_one_word} )
+            return render( request, 'home/home.html', {'words':user_words, 'made_a_search':True, "query":word_data, "def_one_word":def_one_word, 'practice':practice} )
 
-        # IF SAVING THE WORD
+        # If saving the word
         elif( request.POST.get('word') ):
 
             word = request.POST.get('word')
             global_dict_check = all_words.filter(word=word)
 
-            # IF THE WORD IS NOT PRESENT IN THE WHOLE DICTIONARY
+            # If the word is not present in the whole dictionary
             if not global_dict_check:
                 dict_word = Dictword(
                     word = request.POST.get('word'),
                     definition = request.POST.get('definition').replace('-',' '),
                     part_of_speech = request.POST.get('part_of_speech')
                 )
+                # Save first to add user relationship
                 dict_word.save()
                 dict_word.user.add(current_user)
-            # IF WORD IS IN DICTIONARY
+            # If word is in the dictionary
             else:
                 user_dict_check = user_words.filter(word=word)
-                # IF THE WORD IS NOT IN USER'S DICTIONARY
+
+                # If word is not in the user's dictionary
                 if not user_dict_check:
                     global_dict_check.user.add(current_user)
 
-            return render( request, 'home/home.html', {'words':user_words, 'made_a_search':False, "query":None} )
+            # Update user words
+            user_words = all_words.filter(user=current_user).order_by('-id')
+            # Update Practice availability
+            practice = True if len(user_words) > 9 else False
+
+            return render( request, 'home/home.html', {'words':user_words, 'made_a_search':False, "query":None, 'practice': practice} )
         
-    # IF THE USER CLICKS CLEAR AND DOESN'T ADD THE WORD    
-    return render( request, 'home/home.html', {'words':user_words, 'made_a_search':False, "query":None} )
+    # If the user click 'Clear'    
+    return render( request, 'home/home.html', {'words':user_words, 'made_a_search':False, "query":None, 'practice': practice} )
