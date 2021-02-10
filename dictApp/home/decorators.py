886132970ed6d10e13a8dd917ca287ac99d5b951
mehmetsan <trabzonpower@gmail.com>
2021-02-10 13:09:52 +0300

    Comments added, and readability improved

--- dictApp/home/decorators.py
+++ dictApp/home/decorators.py
@@ -2,17 +2,29 @@ from django.core.exceptions import PermissionDenied
 from dictword.models import Dictword
 from django.shortcuts import redirect
 
+'''
+    Decorator for practice page.
+
+    Checks whether the User is authenticated
+
+    If User is logged in, and has words > 9, returns the view function
+
+    Else redirects to the landing page  
+'''
 def user_see_practice(function):
     def wrap(request, *args, **kwargs):
         user = request.user
-        user_words = Dictword.objects.all().filter(user=user)
 
-        practice = True if len(user_words) > 9 else False
+        if user.is_authenticated:
+            
+            user_words = Dictword.objects.all().filter(user=user)
+            practice = True if len(user_words) > 9 else False
 
-        if practice:
-            return function(request, *args, **kwargs)
-        else:
-            return redirect('/')
+            if practice:
+                return function(request, *args, **kwargs)
+
+        return redirect('/')
+
     
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
--- dictApp/home/views.py
+++ dictApp/home/views.py
@@ -11,6 +11,13 @@ import random
 from dotenv import load_dotenv
 from math import sqrt
 
+'''
+    Helper function for API call to WordsAPI
+
+    Takes a word to search (String) as parameter
+
+    Returns the definition and part of speach of the inputted word
+'''
 def get_data(search_word):
     # API Configuration
     url = "https://wordsapiv1.p.rapidapi.com/words/" + search_word + "/definitions"
@@ -25,7 +32,6 @@ def get_data(search_word):
     
     try:
         first_meaning = json_data['definitions'][0]
-
         # Storing only the definition and part of speech
         definition, part_of_speech = first_meaning['definition'], first_meaning['partOfSpeech']
 
@@ -34,13 +40,30 @@ def get_data(search_word):
 
     return definition, part_of_speech
 
+'''
+    A helper method for local dictionary search
+
+    Takes a word (String) as input
+
+    Returns Dictword model instance of the inputted word if its in the dictionary
+
+    Else, returns False
+'''
 def global_check(word):
     all_words = Dictword.objects.all()
     global_dict_check = all_words.filter(word=word.capitalize())
-
     return global_dict_check[0] if global_dict_check else False
 
-def update_score( words, current_user ):
+'''
+    A helper method for updating the score attributes of the words
+
+    Takes a list of words Dictword objects and User instance
+
+    Calculates and updates the new score according to the formula
+
+    Returns None
+'''
+def update_powers( words, current_user ):
     for each in words:
         interaction_obj = Interaction.objects.all().filter(word=each, user=current_user)
         appear_count = interaction_obj[0].appear_count
@@ -51,16 +74,25 @@ def update_score( words, current_user ):
         interaction_obj.update(power=power)
     return
 
+'''
+    Helper function for gathering display informations for User words
+
+    Takes a list of User's dictwords and the User instance as input
+
+    For each word, generates a list of Dictword object, Interaction object, and color
+
+    Decides on the color based on the Interaction's the power
+
+    Returns the list of generated lists
+'''
 def get_powers( words, current_user ):
     powers = []
     for each in words:
-        word_obj = Dictword.objects.get(word=each)
+        word_obj = Dictword.objects.get(word=each.word)
         interaction_obj = Interaction.objects.get(word=each, user=current_user)
      
-        appear_count = interaction_obj.appear_count
-        search_count = interaction_obj.search_count
+        # Decide on the display color
         power = interaction_obj.power
-
         color = ""
 
         if power >= 5:
@@ -70,10 +102,181 @@ def get_powers( words, current_user ):
         else:
             color = 'red'
 
-
-        powers.append([word_obj, interaction_obj.power, color, appear_count, search_count, power])
+        powers.append([word_obj, interaction_obj.power, color ])
     return powers
 
+'''
+    Helper function for searching a word
+
+    Takes request, searched word (String) and list of User's Dictwords
+
+    First searches the word in the locally downloaded data
+
+    Uses API call if the word is not available in database
+
+    Shows page with error message if the API call fails
+
+    Else shows search query results on the page
+
+'''
+def search_new(request, search_word, user_words):
+    load_dotenv('.env')
+    global_dict_check = global_check(search_word)
+    user_dict_check = user_words.filter(word=search_word)
+    display = get_powers( list(user_words) , request.user )
+    definition, part_of_speech = "", ""
+    practice = True if len(user_words) > 9 else False
+    not_found = False
+
+    # If the word is already in the database
+    if global_dict_check:
+        definition = global_dict_check.definition
+        part_of_speech = global_dict_check.part_of_speech
+
+        # Update interaction if the word is in user's dictionary
+        if user_dict_check:
+            interaction = Interaction.objects.all().filter(word=user_dict_check[0], user=request.user)
+            search_count = interaction[0].search_count
+            interaction.update(search_count = search_count + 1)
+
+    # If we need an API call
+    else:
+        definition, part_of_speech = get_data(search_word)
+        # If the entered word lookup failed
+        if not definition:
+            not_found = True
+            return render( request, 'home/home.html', {'words':display, 'made_a_search':True, 'practice':practice, 'not_found':not_found} )
+
+    # Convert definition to one sentence to store its value in hidden input
+    def_one_word = definition.replace(' ','-')
+
+    # For displaying the Searched Word information
+    word_data = {
+        'word' : search_word.capitalize(),
+        'definition' : definition.capitalize(),
+        'part_of_speech' : part_of_speech.capitalize()
+    }
+    
+    return render( request, 'home/home.html', {'words':display, 'made_a_search':True, "query":word_data, "def_one_word":def_one_word, 'practice':practice, 'not_found':False} )
+
+'''
+    Helper function for saving a searched word
+
+    Takes request, searched word (String) and list of User's Dictwords
+
+    Adds the word to the database if it is not already in it 
+
+    If the word is in the database, checks whether it is in the User's dictionary
+
+    If it is not, adds User to the Dictword's users
+
+    Then checks whether user has a Instance of the word
+
+    If it is not, creates and adds and Instance
+
+    Returns the upadeted view 
+
+'''
+def save_word(request, search_word, user_words):
+    word = request.POST.get('word')
+    global_dict_check = global_check(word)
+    user_dict_check = user_words.filter(word=search_word)
+    created_word = ""
+
+    # If the word is not present in the whole dictionary
+    if not global_dict_check:
+        dict_word = Dictword(
+            word = request.POST.get('word'),
+            definition = request.POST.get('definition').replace('-',' ').capitalize(),
+            part_of_speech = request.POST.get('part_of_speech')
+        )
+        # Save first to add user relationship
+        dict_word.save()
+        dict_word.user.add(request.user)
+        created_word = dict_word
+
+    # If word is in the dictionary
+    else:
+        created_word = global_dict_check
+        # If word is not in the user's dictionary
+        if not user_dict_check:
+            global_dict_check.user.add(request.user)
+
+    inter_check = Interaction.objects.all().filter(user=request.user, word=created_word)        
+    if not inter_check:
+        # Create a score for user - word
+        Interaction.objects.create(
+            user=request.user,
+            word=created_word,
+            points = 0,
+            search_count = 1,
+            appear_count = 1,
+            power = 0
+        )
+
+    # Update user words
+    user_words = Dictword.objects.all().filter(user=request.user).order_by('-id')
+    # Update Practice availability
+    practice = True if len(user_words) > 9 else False
+
+    display = get_powers( list(user_words) , request.user )
+    return render( request, 'home/home.html', {'words':display, 'made_a_search':False, 'practice': practice} )
+
+'''
+    Helper funciton for removing a word
+
+    Takes request and word to remove (Dictword) as inputs
+
+    Deletes the Dictword object
+
+    Updates User's words and practice eligibility 
+
+    Returns the upadeted view
+'''
+def remove_word(request, remove_word):
+    print("REMOVE WORD", remove_word)
+    remove_word.user.remove(request.user)
+
+    remove_interaction = Interaction.objects.all().filter(user=request.user, word=remove_word)
+    remove_interaction.delete()
+
+    # Update user words
+    user_words = Dictword.objects.all().filter(user=request.user).order_by('-id')
+
+    # Update Practice availability
+    practice = True if len(user_words) > 9 else False
+
+    user_words = get_powers( list(user_words) , request.user )
+    return render( request, 'home/home.html', {'words':user_words, 'made_a_search':False, 'practice': practice} )
+
+'''
+    Helper function for updating a Interaction's score
+
+    Takes request, a word (String), and a change amount
+
+    Updates the power of the word by the change amount
+
+    Returns None
+
+'''
+def update_score( request, word, change):
+    changed_word = Dictword.objects.get(word=word)
+    changed_word_inter = Interaction.objects.all().filter(user=request.user, word=changed_word)
+    changed_word_points = changed_word_inter[0].points
+    changed_word_inter.update(points=changed_word_points + change)
+    return
+
+
+'''
+    Home view for the landing page
+
+    Requires user login with a decorator
+
+    Displays user words as default
+
+    Returns rendered views based on the incoming POST request
+
+'''
 # Create your views here.
 @login_required(login_url='/users/login')
 def home(request):
@@ -85,113 +288,38 @@ def home(request):
 
     if request.method == "POST":
 
-        #  Using environment variables for API KEY
-        load_dotenv('.env')
-
         search_word = request.POST.get('search_word')
 
         # If searching a new word
         if search_word:
-            global_dict_check = global_check(search_word)
-            user_dict_check = user_words.filter(word=search_word)
-
-            definition, part_of_speech = "", ""
-
-            # If the word is already in the database
-            if global_dict_check:
-                definition = global_dict_check.definition
-                part_of_speech = global_dict_check.part_of_speech
-
-                # Update interaction if the word is in user's dictionary
-                if user_dict_check:
-                    interaction = Interaction.objects.all().filter(word=user_dict_check[0], user=current_user)
-                    search_count = interaction[0].search_count
-                    interaction.update(search_count = search_count + 1)
-
-            # If we need an API call
-            else:
-                definition, part_of_speech = get_data(search_word)
-                if not definition:
-                    user_words = get_powers( list(user_words) , request.user )
-                    return render( request, 'home/home.html', {'words':user_words, 'made_a_search':True, 'practice':practice, 'not_found':True} )
-
-            # Convert definition to one sentence to store its value in hidden input
-            def_one_word = definition.replace(' ','-')
-
-            # For displaying the Searched Word information
-            word_data = {
-                'word' : search_word.capitalize(),
-                'definition' : definition.capitalize(),
-                'part_of_speech' : part_of_speech.capitalize()
-            }
-            user_words = get_powers( list(user_words) , request.user )
-            return render( request, 'home/home.html', {'words':user_words, 'made_a_search':True, "query":word_data, "def_one_word":def_one_word, 'practice':practice} )
+            return search_new(request, search_word, user_words)
 
         # If saving the word
         elif( request.POST.get('word') ):
+            return save_word(request, search_word, user_words)
 
-            word = request.POST.get('word')
-            global_dict_check = global_check(word)
-            user_dict_check = user_words.filter(word=search_word)
-            created_word = ""
-
-            # If the word is not present in the whole dictionary
-            if not global_dict_check:
-                dict_word = Dictword(
-                    word = request.POST.get('word'),
-                    definition = request.POST.get('definition').replace('-',' ').capitalize(),
-                    part_of_speech = request.POST.get('part_of_speech')
-                )
-                # Save first to add user relationship
-                dict_word.save()
-                dict_word.user.add(current_user)
-                created_word = dict_word
-
-            # If word is in the dictionary
-            else:
-                created_word = global_dict_check
-                # If word is not in the user's dictionary
-                if not user_dict_check:
-                    global_dict_check.user.add(current_user)
-                    
-            # Create a score for user - word
-            Interaction.objects.create(
-                user=current_user,
-                word=created_word,
-                points = 0,
-                search_count = 1,
-                appear_count = 1,
-                power = 0
-            )
-
-            # Update user words
-            user_words = all_words.filter(user=current_user).order_by('-id')
-            # Update Practice availability
-            practice = True if len(user_words) > 9 else False
-
-            user_words = get_powers( list(user_words) , request.user )
-            return render( request, 'home/home.html', {'words':user_words, 'made_a_search':False, "query":None, 'practice': practice} )
-        
         # If removing a word
         elif( request.POST.get('word_to_remove')):
-            remove_word = global_check(request.POST.get('word_to_remove'))
-            remove_word.user.remove(current_user)
+            word_to_remove = global_check(request.POST.get('word_to_remove'))
+            return remove_word(request, word_to_remove)
 
-            remove_interaction = Interaction.objects.all().filter(user=current_user, word=remove_word)
-            remove_interaction.delete()
-            # Update user words
-            user_words = all_words.filter(user=current_user).order_by('-id')
-
-            # Update Practice availability
-            practice = True if len(user_words) > 9 else False
-
-            user_words = get_powers( list(user_words) , request.user )
-            return render( request, 'home/home.html', {'words':user_words, 'made_a_search':False, "query":None, 'practice': practice} )
-
-    user_words = get_powers( list(user_words) , request.user )
     # If the user click 'Clear'    
-    return render( request, 'home/home.html', {'words':user_words, 'made_a_search':False, "query":None, 'practice': practice, "word_stats":list(all_words.filter(user=current_user).order_by('-id'))} )
+    display = get_powers( list(user_words) , current_user )
 
+    return render( request, 'home/home.html', {'words':display, 'made_a_search':False, 'practice': practice } )
+
+
+'''
+    Practice view for the practicing page
+
+    Requires User practice eligiblity, else redirects to landing page
+
+    Selects a random definition and 3 word choices and displays them
+
+    If the user gives an answer, evaluates the answer and makes the required score changes
+
+    Returns the rendered practice page view
+'''
 @user_see_practice
 def practice(request):
 
@@ -202,38 +330,27 @@ def practice(request):
         question_fw2 = request.POST.get('question_fw2')
         user_answer = request.POST.get('user_answer')
 
-        # Correct answer given
+        # If correct answer is given
         if user_answer == question_cw:
-            cw_word = Dictword.objects.get(word=question_cw)
-            cw_score = Interaction.objects.all().filter(user=request.user, word=cw_word)
-            cw_points = cw_score[0].points
-            cw_score.update(points=cw_points + 4)
+            update_score(request,question_cw,3)
 
-        # Wrong answer given
+        # If wrong answer is given
         else:
             # Subtract from the correct answer
-            cw_word = Dictword.objects.get(word=question_cw)
-            cw_score = Interaction.objects.all().filter(user=request.user, word=cw_word)
-            cw_points = cw_score[0].points
-            cw_score.update(points=cw_points - 2)
+            update_score(request,question_cw,-2)
 
             # Subtract from first false word's points
-            fw1_word = Dictword.objects.get(word=question_fw1)
-            fw1_score = Interaction.objects.all().filter(user=request.user, word=fw1_word)
-            fw1_points = fw1_score[0].points
-            fw1_score.update(points=fw1_points - 1)
+            update_score(request,question_fw1,-1)
 
             # Subtract from second false word's points
-            fw2_word = Dictword.objects.get(word=question_fw2)
-            fw2_score = Interaction.objects.all().filter(user=request.user, word=fw2_word)
-            fw2_points = fw2_score[0].points
-            fw2_score.update(points=fw2_points - 1)
+            update_score(request,question_fw2,-1)
+
 
     # Get random words and a random definition
     user_words = Dictword.objects.all().filter(user=request.user)
     random_words = random.sample(list(user_words), 3)
 
-    update_score( list(user_words), request.user)
+    update_powers( list(user_words), request.user)
 
     # Update each word's appear counts
     for each in random_words:
@@ -246,7 +363,7 @@ def practice(request):
     false_word1 = random_words[1].word
     false_word2 = random_words[2].word
 
-    # So that correct answer changes every time
+    # So that correct answer's place changes every time
     choices = [random_word, false_word1, false_word2 ]
     random.shuffle(choices)
 
